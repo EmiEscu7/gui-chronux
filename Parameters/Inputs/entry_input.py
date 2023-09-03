2344a0b5ad1213675ae2e6f1191ded4979e87d24
EmiEscu7 <emi.escudero1998@gmail.com>
2023-09-03 11:02:38 -0300

    add label to entry inputs

--- Parameters/Inputs/entry_input.py
+++ Parameters/Inputs/entry_input.py
@@ -25,11 +25,32 @@ class EntryInput(Input):
         self._placeholder_text_color = placeholder_text_color
         self._placeholder_text = placeholder_text
 
-    def get_item(self) -> ctk.CTkEntry:
-        return ctk.CTkEntry(
+    def get_item(self) -> ctk.CTkFrame:
+        frame = ctk.CTkFrame(
             master=self.master,
-            textvariable=self._textvariable,
             width=self.width,
+            height=self.height,
+            fg_color='transparent',
+            border_width=0,
+            corner_radius=0,
+        )
+
+        label = ctk.CTkLabel(
+            master=frame,
+            width=int(self.width/2)-10,
+            height=self.height,
+            text=self._placeholder_text,
+            text_color=self.text_color,
+            corner_radius=0,
+            justify='left',
+            font=self.font
+        )
+        label.grid(row=0, column=0)
+
+        entry = ctk.CTkEntry(
+            master=frame,
+            textvariable=self._textvariable,
+            width=int(self.width/2)-10,
             height=self.height,
             corner_radius=self.corner_radius,
             fg_color=self.fg_color,
@@ -39,3 +60,6 @@ class EntryInput(Input):
             font=self.font,
             state=self.state,
         )
+        entry.grid(row=0, column=1)
+
+        return frame
