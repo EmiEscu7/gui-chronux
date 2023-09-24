3a2f06fd9bd8e28a43a3ddd4e082d5c69bcb4c69
EmiEscu7 <emi.escudero1998@gmail.com>
2023-09-24 12:50:24 -0300

    change parameter type on coherence

--- Parameters/coherence_parameters.py
+++ Parameters/coherence_parameters.py
@@ -8,12 +8,14 @@ class CoherenceParameters(Parameters):
             self,
             signals: Tuple[int, str, List[str], any] = None,
             check_all_signals: Tuple[int, str, bool, any] = None,
-            taper1: Tuple[int, str, any, any] = None,
-            taper2: Tuple[int, str, any, any] = None,
+            # taper1: Tuple[int, str, any, any] = None,
+            # taper2: Tuple[int, str, any, any] = None,
+            tapers: Tuple[int, str, any, any, any] = None,
             sample_frequency: Tuple[int, str, any, any] = None,
             frequencies: Tuple[int, str, any, any] = None,
-            freq_pass1: Tuple[int, str, any, any] = None,
-            freq_pass2: Tuple[int, str, any, any] = None,
+            # freq_pass1: Tuple[int, str, any, any] = None,
+            # freq_pass2: Tuple[int, str, any, any] = None,
+            freq_pass: Tuple[int, str, any, any, any] = None,
             time1: Tuple[int, str, any, any] = None,
             time2: Tuple[int, str, any, any] = None,
             trialave: Tuple[int, str, any, any] = None,
@@ -22,12 +24,14 @@ class CoherenceParameters(Parameters):
         super().__init__()
         self._signals = signals
         self._check_all_signals = check_all_signals
-        self._taper1 = taper1
-        self._taper2 = taper2
+        # self._taper1 = taper1
+        # self._taper2 = taper2
+        self._tapers = tapers
         self._sample_frequency = sample_frequency
         self._frequencies = frequencies
-        self._freq_pass1 = freq_pass1
-        self._freq_pass2 = freq_pass2
+        # self._freq_pass1 = freq_pass1
+        # self._freq_pass2 = freq_pass2
+        self._freq_pass = freq_pass
         self._time1 = time1
         self._time2 = time2
         self._trialave = trialave
@@ -35,19 +39,19 @@ class CoherenceParameters(Parameters):
 
     def load_params(self, master, attrbs) -> List[
         Union[CTkCheckBox, CTkComboBox, CTkRadioButton, CTkTextbox, CTkEntry]]:
-        atribs = [self._signals, self._check_all_signals, self._taper1, self._taper2, self._sample_frequency, self._frequencies, self._freq_pass1, self._freq_pass2, self._time1, self._time2, self._trialave, self._err]
+        atribs = [self._signals, self._check_all_signals, self._tapers, self._sample_frequency, self._frequencies, self._freq_pass, self._time1, self._time2, self._trialave, self._err]
         return super().load_params(master, atribs)
 
     def get_data_params(self) -> Dict:
         return {
             'signal': str(self.popups_multiple[self._signals[1]].get()),
             'all': self.dict_checks[self._check_all_signals[1]].get(),
-            'taper1': int(self.entries[self._taper1[1]].get()),
-            'taper2': int(self.entries[self._taper2[1]].get()),
+            'taper1': int(self.entry_range[self._tapers[1]][0].get()),
+            'taper2': int(self.entry_range[self._tapers[1]][1].get()),
             'fs': int(self.entries[self._sample_frequency[1]].get()),
             'freq': float(self.popups[self._frequencies[1]].get()),
-            'freq_pass1': int(self.entries[self._freq_pass1[1]].get()),
-            'freq_pass2': int(self.entries[self._freq_pass2[1]].get()),
+            'freq_pass1': int(self.entry_range[self._freq_pass[1]][0].get()),
+            'freq_pass2': int(self.entry_range[self._freq_pass[1]][1].get()),
             'time1': str(self.popups[self._time1[1]].get()),
             'time2': str(self.popups[self._time2[1]].get()),
             'trialave': int(self.entries[self._trialave[1]].get()),
