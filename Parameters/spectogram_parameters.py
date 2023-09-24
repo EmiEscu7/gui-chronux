9ee16b6dcb94c36f2e082d590bcb81ddac226a8f
EmiEscu7 <emi.escudero1998@gmail.com>
2023-09-24 12:46:43 -0300

    change parameter type on spectogram

--- Parameters/spectogram_parameters.py
+++ Parameters/spectogram_parameters.py
@@ -9,14 +9,17 @@ class SpectogramParameters(Parameters):
             self,
             signals: Tuple[int, str, List[str], any] = None,
             check_all_signals: Tuple[int, str, bool, any] = None,
-            movingwin1: Tuple[int, str, any, any] = None,
-            movingwin2: Tuple[int, str, any, any] = None,
-            taper1: Tuple[int, str, any, any] = None,
-            taper2: Tuple[int, str, any, any] = None,
+            # movingwin1: Tuple[int, str, any, any] = None,
+            # movingwin2: Tuple[int, str, any, any] = None,
+            movingwin: Tuple[int, str, any, any, any] = None,
+            # taper1: Tuple[int, str, any, any] = None,
+            # taper2: Tuple[int, str, any, any] = None,
+            tapers: Tuple[int, str, any, any, any] = None,
             sample_frequency: Tuple[int, str, any, any] = None,
             frequencies: Tuple[int, str, any, any] = None,
-            freq_pass1: Tuple[int, str, any, any] = None,
-            freq_pass2: Tuple[int, str, any, any] = None,
+            # freq_pass1: Tuple[int, str, any, any] = None,
+            # freq_pass2: Tuple[int, str, any, any] = None,
+            freq_pass: Tuple[int, str, any, any, any] = None,
             time1: Tuple[int, str, any, any] = None,
             time2: Tuple[int, str, any, any] = None,
             trialave: Tuple[int, str, any, any] = None,
@@ -25,14 +28,17 @@ class SpectogramParameters(Parameters):
         super().__init__()
         self._signals = signals
         self._check_all_signals = check_all_signals
-        self._movingwin1 = movingwin1
-        self._movingwin2 = movingwin2
-        self._taper1 = taper1
-        self._taper2 = taper2
+        # self._movingwin1 = movingwin1
+        # self._movingwin2 = movingwin2
+        self._movingwin = movingwin
+        # self._taper1 = taper1
+        # self._taper2 = taper2
+        self._tapers = tapers
         self._sample_frequency = sample_frequency
         self._frequencies = frequencies
-        self._freq_pass1 = freq_pass1
-        self._freq_pass2 = freq_pass2
+        # self._freq_pass1 = freq_pass1
+        # self._freq_pass2 = freq_pass2
+        self._freq_pass = freq_pass
         self._time1 = time1
         self._time2 = time2
         self._trialave = trialave
@@ -40,8 +46,8 @@ class SpectogramParameters(Parameters):
 
     def load_params(self, master, attrbs) -> List[
         Union[CTkCheckBox, CTkComboBox, CTkRadioButton, CTkTextbox, CTkEntry]]:
-        atribs = [self._signals, self._check_all_signals, self._movingwin1, self._movingwin2, self._taper1,
-                  self._taper2, self._sample_frequency, self._frequencies, self._freq_pass1, self._freq_pass2,
+        atribs = [self._signals, self._check_all_signals, self._movingwin, self._tapers,
+                  self._sample_frequency, self._frequencies, self._freq_pass,
                   self._time1, self._time2, self._trialave, self._err]
         return super().load_params(master, atribs)
 
@@ -49,14 +55,14 @@ class SpectogramParameters(Parameters):
         return {
             'signal': str(self.popups_multiple[self._signals[1]].get()),
             'all': self.dict_checks[self._check_all_signals[1]].get(),
-            'movingwin1': float(self.entries[self._movingwin1[1]].get()),
-            'movingwin2': float(self.entries[self._movingwin2[1]].get()),
-            'taper1': int(self.entries[self._taper1[1]].get()),
-            'taper2': int(self.entries[self._taper2[1]].get()),
+            'movingwin1': float(self.entry_range[self._movingwin[1]][0].get()),
+            'movingwin2': float(self.entry_range[self._movingwin[1]][1].get()),
+            'taper1': int(self.entry_range[self._tapers[1]][0].get()),
+            'taper2': int(self.entry_range[self._tapers[1]][1].get()),
             'fs': int(self.entries[self._sample_frequency[1]].get()),
             'freq': float(self.popups[self._frequencies[1]].get()),
-            'freq_pass1': int(self.entries[self._freq_pass1[1]].get()),
-            'freq_pass2': int(self.entries[self._freq_pass2[1]].get()),
+            'freq_pass1': int(self.entry_range[self._freq_pass[1]][0].get()),
+            'freq_pass2': int(self.entry_range[self._freq_pass[1]][1].get()),
             'time1': str(self.popups[self._time1[1]].get()),
             'time2': str(self.popups[self._time2[1]].get()),
             'trialave': int(self.entries[self._trialave[1]].get()),
